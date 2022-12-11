09d14a6b2bea8e6193fbed9fee62810aa6a1d2c7
Dmitry Sirotin <55099210+Sirotin-D@users.noreply.github.com>
2022-12-11 19:43:33 +0300

    Add main menu for Android app (#6)
    * Draft version tab bar
    
    * Update module names
    
    * Add main tab bar with navigation
    
    * Update android app manifest
    
    Co-authored-by: Sirotin-D <dimsirotin151000@gmail.com>

--- Android/app/src/main/java/com/example/tutor_online/ui/activity/MainActivity.kt
+++ Android/app/src/main/java/com/example/tutor_online/ui/activity/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.tutor_online.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.tutor_online.R
+import com.example.tutor_online.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navView: BottomNavigationView = findViewById(R.id.main_tab_bar)
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.my_profile_fragment,
+                R.id.lessons_list_fragment,
+                R.id.my_lessons_list_fragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
---
+++ Android/app/src/main/java/com/example/tutor_online/ui/fragment/LessonListFragment.kt
@@ -0,0 +1,18 @@
+package com.example.tutor_online.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.tutor_online.R
+
+class LessonListFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.lessons_list_fragment, container, false)
+    }
+}
